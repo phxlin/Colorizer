e8491933e5bbc7279e268d7620fdb49347b597d1
phxlin <yfln11@gmail.com>
2018-02-22 18:33:29 -0500

    Color class and single color layout

---
+++ app/src/main/java/me/yufanlin/colorexplorer/model/Color.java
@@ -0,0 +1,53 @@
+package me.yufanlin.colorexplorer.model;
+
+public class Color {
+
+    private float hue;
+    private float saturation;
+    private float value;
+
+    //Constructors
+    public Color() {
+    }
+
+    public Color(float hue, float saturation, float value) {
+        this.hue = hue;
+        this.saturation = saturation;
+        this.value = value;
+    }
+
+    //Getters and setters
+    public float getHue() {
+        return hue;
+    }
+
+    public void setHue(float hue) {
+        this.hue = hue;
+    }
+
+    public float getSaturation() {
+        return saturation;
+    }
+
+    public void setSaturation(float saturation) {
+        this.saturation = saturation;
+    }
+
+    public float getValue() {
+        return value;
+    }
+
+    public void setValue(float value) {
+        this.value = value;
+    }
+
+    //To string
+    @Override
+    public String toString() {
+        return "Color{" +
+                "hue=" + hue +
+                ", saturation=" + saturation +
+                ", value=" + value +
+                '}';
+    }
+}
